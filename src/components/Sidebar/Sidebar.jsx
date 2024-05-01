a552781266cea2e505d89a2d439449d39e68fc58
Maria Freitas <mariaeduardamartinsf@gmail.com>
2024-05-01 00:25:39 -0300

    feat; novas atualizacoes de layout, conteudo e imagens

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,34 @@
+import './sidebar.css'
+import {assets} from '../../assets/assets'
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img className='menu'
+        src={assets.menu_icon} alt="" />
+        <div className='new-chat'>
+          <img src={assets.plus_icon} alt="" />
+           <p>New Chat</p>
+        </div>
+        <div className="recent">
+          <p className="recent-title">
+            Recent
+          </p>
+          <div className="recent-entry">
+            <img src={assets.gemini_icon} alt="" />
+          <p>What is react...</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          <p>Help</p>
+        </div>
+      </div>
+      </div>
+  )
+}
+
+export default Sidebar
