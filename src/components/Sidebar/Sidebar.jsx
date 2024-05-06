53153cc5014f57d681c48f7d570687d1e4354692
Maria Freitas <mariaeduardamartinsf@gmail.com>
2024-05-06 16:48:12 -0300

    feat: nova atualizacao em sessao de menu para abrir e fechar o mesmo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,23 @@
 import './sidebar.css'
 import {assets} from '../../assets/assets'
+import { useState } from 'react'
 
 function Sidebar() {
+
+const [extended, setExtended] = useState(false)
+
   return (
     <div className="sidebar">
       <div className="top">
         <img className='menu'
+        onClick={() => setExtended(prev => !prev)}
         src={assets.menu_icon} alt="" />
         <div className='new-chat'>
           <img src={assets.plus_icon} alt="" />
-           <p>New Chat</p>
+          {extended ? <p>New Chat</p> : null}
+
         </div>
+        {extended ?         
         <div className="recent">
           <p className="recent-title">
             Recent
@@ -19,23 +26,29 @@ function Sidebar() {
             <img src={assets.message_icon} alt="" />
           <p>What is react...</p>
           </div>
-        </div>
+        </div> : null}
+
       </div>
 
       <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="" />
-          <p>Help</p>
+          {extended ?          
+           <p>Help</p> : null}
         </div>
 
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="" />
-          <p>Activity</p>
+          {extended ?          
+           <p>Activity</p> : null}
+
         </div>
 
         <div className="bottom-item recent-entry">
           <img src={assets.send_icon} alt="" />
-          <p>Settings</p>
+          {extended ?          
+           <p>Settings</p> : null}
+
         </div>
       </div>
       </div>
